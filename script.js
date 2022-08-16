dd134d265d523c69ebaf505c6f6adeb11ec76042
ashsass <93344091+ashsass@users.noreply.github.com>
2022-08-16 18:05:31 -0400

    Structure the buttons in project section. When clicked content appears, clicked again it disappears.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /*Variables*/
-let projImg1 = document.getElementById('project1');
-let projImg2 = document.getElementById('project2');
-let projImg3 = document.getElementById('project3');
+let projImg1 = document.getElementById('project-1');
+let projImg2 = document.getElementById('project-2');
+let projImg3 = document.getElementById('project-3');
 
-let projPara1 = document.getElementById('mixedmessages');
-let projPara2 = document.getElementById('bookclub');
-let projPara3 = document.getElementById('meowpeow');
+let projPara1 = document.getElementById('paragraph-1');
+let projPara2 = document.getElementById('paragraph-2');
+let projPara3 = document.getElementById('paragraph-3');
 
 /*Hidden elements*/
 projImg1.hidden = true;
@@ -15,3 +15,19 @@ projImg3.hidden = true;
 projPara1.hidden = true;
 projPara2.hidden = true;
 projPara3.hidden = true;
+
+/*Event handlers to show the project when clicked*/
+function toggleProject(img, para){
+    let hiddenImage = img.hidden;
+    let hiddenParagraph = para.hidden;
+
+    if(hiddenImage && hiddenParagraph){
+        img.hidden = false;
+        para.hidden = false;
+    }
+    else if(!(hiddenImage && hiddenParagraph)){
+        img.hidden = true;
+        para.hidden = true;
+    }
+}
+
