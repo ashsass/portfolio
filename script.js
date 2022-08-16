49b0416ae63de080bdde1dc65045669bf2c41b60
ashsass <93344091+ashsass@users.noreply.github.com>
2022-08-16 17:08:50 -0400

    Create variables

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Variables*/
+let projImg1 = document.getElementsById('project1');
+let projImg2 = document.getElementsById('project2');
+let projImg3 = document.getElementsById('project3');
+
+let projPara1 = document.getElementsById('mixedmessages');
+let projPara2 = document.getElementsById('bookclub');
+let projPara2 = document.getElementsById('meowpeow');
